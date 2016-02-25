bd5b8006d8d21bdef9418acf34c384dc47d2f0b7
masterUNG <phrombutr@gmail.com>
2016-02-25 10:14:55 +0700

    NoAction Bar Finish

--- app/src/main/java/appewtc/masterung/dreamgun/MyManage.java
+++ app/src/main/java/appewtc/masterung/dreamgun/MyManage.java
@@ -12,6 +12,16 @@ public class MyManage {
     private MyOpenHelper myOpenHelper;
     private SQLiteDatabase writeSqLiteDatabase, readSqLiteDatabase;
 
+    //About userTABLE
+    public static final String table_user = "userTABLE";
+    public static final String column_id = "_id";
+    public static final String column_User = "User";
+    public static final String column_Password = "Password";
+    public static final String column_Name = "Name";
+    public static final String column_Email = "Email";
+    public static final String column_Birth = "Birth";
+    public static final String column_Country = "Country";
+
 
     public MyManage(Context context) {
 
