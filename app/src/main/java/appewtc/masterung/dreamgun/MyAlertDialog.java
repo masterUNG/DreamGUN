6ec9531f731abab0e0ab3a173fa8d0f21a56e913
masterUNG <phrombutr@gmail.com>
2016-02-25 11:57:17 +0700

    Check Space Finish

---
+++ app/src/main/java/appewtc/masterung/dreamgun/MyAlertDialog.java
@@ -0,0 +1,29 @@
+package appewtc.masterung.dreamgun;
+
+import android.app.AlertDialog;
+import android.content.Context;
+import android.content.DialogInterface;
+
+/**
+ * Created by masterUNG on 2/25/16 AD.
+ */
+public class MyAlertDialog {
+
+    public void myDialog(Context context, String strTitle, String strMessage) {
+
+        AlertDialog.Builder builder = new AlertDialog.Builder(context);
+        builder.setIcon(R.drawable.danger);
+        builder.setTitle(strTitle);
+        builder.setMessage(strMessage);
+        builder.setCancelable(false);
+        builder.setPositiveButton("OK", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int i) {
+                dialogInterface.dismiss();
+            }
+        });
+        builder.show();
+
+    }   // myDialog
+
+}   // Main Class
--- app/src/main/java/appewtc/masterung/dreamgun/SignUpActivity.java
+++ app/src/main/java/appewtc/masterung/dreamgun/SignUpActivity.java
@@ -1,13 +1,74 @@
 package appewtc.masterung.dreamgun;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.DatePicker;
+import android.widget.EditText;
+import android.widget.Spinner;
 
 public class SignUpActivity extends AppCompatActivity {
+
+    //Explicit
+    private EditText userEditText, passwordEditText,
+            nameEditText, emailEditText;
+    private DatePicker birthDatePicker;
+    private Spinner countrySpinner;
+    private String userString, passwordString, nameString, emailString,
+            birthString, countryString;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_up);
+
+        //Bind Widget
+        bindWidget();
+
+    }   // Main Method
+
+    private void bindWidget() {
+
+        userEditText = (EditText) findViewById(R.id.editText3);
+        passwordEditText = (EditText) findViewById(R.id.editText4);
+        nameEditText = (EditText) findViewById(R.id.editText5);
+        emailEditText = (EditText) findViewById(R.id.editText6);
+
+    }   // bindWidget
+
+    public void clickSaveData(View view) {
+
+        //Get Value From Editext
+        userString = userEditText.getText().toString().trim();
+        passwordString = passwordEditText.getText().toString().trim();
+        nameString = nameEditText.getText().toString().trim();
+        emailString = emailEditText.getText().toString().trim();
+
+        //Check Complete
+        if (checkSpace()) {
+            //Have Space
+            MyAlertDialog myAlertDialog = new MyAlertDialog();
+            myAlertDialog.myDialog(SignUpActivity.this, "Have Space", "Please Fill Every Blank");
+
+        } else {
+            //No Space
+
+
+        }   // if
+
+
+
+    }   // clickSaveData
+
+    private boolean checkSpace() {
+
+        boolean bolSpace = true; //Have Space
+
+        bolSpace = userString.equals("") || passwordString.equals("") ||
+                nameString.equals("") || emailString.equals("");
+
+        return bolSpace;
     }
-}
+
+}   // Main Class
